639e3d448f72c6e7d4cef4640fc1369740d5d682
EssD <akinyemisaed@gmail.com>
2023-09-06 12:38:30 +0100

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """Read a text file and returns the list of to-do items"""
+    with open('todos.txt', 'r') as file_local:
+         todos_local = file_local.readlines()
+         return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write thre to-do items  in the text file."""
+    with open (filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+if  __name__ == "__main__":
+    print("Hello")
+    print(get_todos)
